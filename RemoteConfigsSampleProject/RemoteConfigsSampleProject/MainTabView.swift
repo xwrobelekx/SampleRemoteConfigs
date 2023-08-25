05771eaed3e3af2ca7715e6770eb39befe6aba88
Kamil Wrobel <wrobelk@live.com>
2023-08-25 14:22:10 -0400

    Adds tab bar with new view gated by feature flags

--- RemoteConfigsSampleProject/RemoteConfigsSampleProject/MainTabView.swift
+++ RemoteConfigsSampleProject/RemoteConfigsSampleProject/MainTabView.swift
@@ -9,20 +9,46 @@ import SwiftUI
 
 struct MainTabView: View {
     @ObservedObject var viewModel: MainTabViewModel
+    @State private var selectedTab = 1
     
     var body: some View {
-        ZStack{
-            Color.mint
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .font(.largeTitle)
-                .foregroundColor(.white)
-        }.ignoresSafeArea()
+        TabView(selection: $selectedTab) {
+            if viewModel.appConfig.isGreenScreenEnabled {
+                GreenScreen()
+                    .tabItem {
+                        Image(systemName: "g.circle.fill")
+                        Text("Green")
+                    }.tag(1)
+            }
+
+            if viewModel.appConfig.isYellowScreenEnabled {
+                YellowScreen()
+                    .tabItem {
+                        Image(systemName: "y.circle.fill")
+                        Text("Yellow")
+                    }
+                    .tag(2)
+            }
+
+            if viewModel.appConfig.isRedScreenEnabled {
+                RedScreen()
+                    .tabItem {
+                        Image(systemName: "r.circle.fill")
+                        Text("Red")
+                    }
+                    .tag(3)
+            }
+            
+        }.accentColor(Color.primary)
+            .onAppear() {
+                UITabBar.appearance().isTranslucent = false
+            }
     }
 }
 
-struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView(viewModel: MainTabViewModel(
-            appConfig: AppConfigManager().appConfig))
-    }
-}
+//struct MainTabView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MainTabView(viewModel: MainTabViewModel(
+//            appConfig: AppConfigManager().appConfig))
+//    }
+//}
