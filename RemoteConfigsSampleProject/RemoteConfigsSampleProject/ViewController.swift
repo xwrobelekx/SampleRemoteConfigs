b4be6ee4186a4813fc557bec9a086df92cd13896
Kamil Wrobel <wrobelk@live.com>
2023-08-24 21:05:47 -0400

    Adds new Xcode sample project

---
+++ RemoteConfigsSampleProject/RemoteConfigsSampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteConfigsSampleProject
+//
+//  Created by Kamil Wrobel on 8/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
